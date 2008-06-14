8f0a22ab39fec49c8c3b12e68ca53840ec6f6efb
Jonny Heggheim <hegjon@gmail.com>
2008-06-14 12:25:25 +0200

    Added config files

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+  def get_filter(map, pattern, controller, action)
+    map.connect pattern, :controller => controller, :action => action, :conditions => { :method => :get }
+  end
+  
+ActionController::Routing::Routes.draw do |map|
+  #default CRUD
+  map.connect '/:controller/',    :action => "list",    :conditions => { :method => :get }
+  map.connect '/:controller/',    :action => "create",  :conditions => { :method => :post }
+  map.connect '/:controller/:id', :action => "show",    :conditions => { :method => :get }
+  map.connect '/:controller/:id', :action => "update",  :conditions => { :method => :put }
+  map.connect '/:controller/:id', :action => "destroy", :conditions => { :method => :delete }
+  
+  #additional read-filter
+  get_filter map, "/camp/:camp_id/barrack/",    "barrack", "list_by_camp"
+  get_filter map, "/barrack/:barrack_id/room/", "room",    "list_by_barrack"
+  get_filter map, "/room/:room_id/booking/",    "booking", "list_by_room"
+end
