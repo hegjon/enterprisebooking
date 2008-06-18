3fec8f39a85ef0966b934eef937e46f4f23cd981
Jonny Heggheim <hegjon@gmail.com>
2008-06-18 17:32:04 +0200

    Added filter booking by arrival and departure

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,10 @@ ActionController::Routing::Routes.draw do |map|
   map.connect '/:controller/:id', :action => "destroy", :conditions => { :method => :delete }
   
   #additional read-filter
-  get_filter map, "/camp/:camp_id/barrack/",    "barrack", "list_by_camp"
-  get_filter map, "/barrack/:barrack_id/room/", "room",    "list_by_barrack"
-  get_filter map, "/room/:room_id/booking/",    "booking", "list_by_room"
+  get_filter map, "/camp/:camp_id/barrack/",       "barrack", "list_by_camp"
+  get_filter map, "/barrack/:barrack_id/room/",    "room",    "list_by_barrack"
+  get_filter map, "/room/:room_id/booking/",       "booking", "list_by_room"
+  
+  get_filter map, "/booking/?arrival=:arrival",     "booking", "list_by_arrival"
+  get_filter map, "/booking/?departure=:departure", "booking", "list_by_arrival"  
 end
