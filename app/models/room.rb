1c8bc96c13cdb9c33e798bca14c0f4696aa867af
Jonny Heggheim <hegjon@gmail.com>
2008-06-18 14:32:14 +0200

    Added support for reservation

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,6 @@
 class Room < ActiveRecord::Base
   belongs_to :barrack
   has_many :bookings
+  #has_many :reservations
+  has_and_belongs_to_many :reservations, :class_name => "Person",  :join_table => "reservations"
 end
