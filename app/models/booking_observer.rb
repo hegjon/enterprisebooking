eaa9c86b527e431e8c28300ed0c1342783d6dac9
Jonny Heggheim <hegjon@gmail.com>
2008-06-18 14:26:47 +0200

    Refactoring + more checks

--- app/models/booking_observer.rb
+++ app/models/booking_observer.rb
@@ -1,15 +1,33 @@
 class BookingObserver < ActiveRecord::Observer
   def before_save(booking)
+    time_conflict(booking)
+    active_conflict(booking)
+  end
+  
+  private
+  
+  def time_conflict(booking)
     raise "Departure is before arrival" if booking.arrival > booking.departure
     
-    b = Booking.find(:first, :conditions => ['id != ifnull(?, -1) and person_id=? and arrival < ? and departure > ?', 
+    b = Booking.first(:conditions => ['id != ifnull(?, -1) and person_id=? and arrival < ? and departure > ?', 
         booking.id, booking.person_id, booking.departure, booking.arrival])
 
-    raise "Person conflict" if b
+    raise PersonConflict.new(b) if b
     
-    b = Booking.find(:first, :conditions => ['id != ifnull(?, -1) and room_id=? and arrival < ? and departure > ?', 
+    if booking.room
+      count = Booking.count(:conditions => ['id != ifnull(?, -1) and room_id=? and arrival < ? and departure > ?', 
           booking.id, booking.room_id, booking.departure, booking.arrival])    
-    
-    raise "Room conflict" if b
+
+      raise "Room conflict" if count >= booking.room.beds 
+    end
+  end
+  
+  def active_conflict(booking)
+    if booking.status == 20
+      b = Booking.first(:conditions => ['id != ifnull(?, -1) and person_id=? and status=20', 
+        booking.id, booking.person_id])
+      
+      raise "Person already active" if b
+    end
   end
 end
