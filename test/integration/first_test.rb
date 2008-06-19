f1a73bfb565ccc9045369ac377237fcb5e2fa98b
Jonny Heggheim <hegjon@gmail.com>
2008-06-19 10:59:54 +0200

    Added test_no_user

--- test/integration/first_test.rb
+++ test/integration/first_test.rb
@@ -3,11 +3,17 @@ require "#{File.dirname(__FILE__)}/../test_helper"
 class FirstTest < ActionController::IntegrationTest
   fixtures :camps
 
-  def test_unauthorized
-    get "/camp/"
-    assert_response :unauthorized
+  def test_no_user
+    get "/camp/", nil
+    assert_response :error
   end
   
+  def test_unauthorized
+    username_password = Base64.encode64("user_dont_exsist:blabla235566");
+    get "/camp/", nil, {"Authorization" => "Basic #{username_password}"}
+    assert_response :error
+  end
+
   def test_authorized
     username_password = Base64.encode64("jonny:ponny");
     get "/camp/", nil, {"Authorization" => "Basic #{username_password}"}
